fe05afd528d9219bc1f832400b11b72bf17faf1b
avinash273 <avinash.shanker@mavs.uta.edu>
2020-12-12 22:12:50 -0600

    checkpoint like and image upload to amazon s3

--- App.tsx
+++ App.tsx
@@ -21,6 +21,7 @@ function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  //need to change this image
   const getRandomImage = () => {
     return 'https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70'
   }
